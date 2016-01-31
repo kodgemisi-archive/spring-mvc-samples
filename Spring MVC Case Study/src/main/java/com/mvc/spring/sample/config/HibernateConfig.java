d44d18a4c1493c7608c1d5b9d2b34f10f163d0f4
Zafer CAKMAK <zafer@kodgemisi.com>
2016-01-31 14:35:01 +0200

    hibernate deals with db creation

--- Spring MVC Case Study/src/main/java/com/mvc/spring/sample/config/HibernateConfig.java
+++ Spring MVC Case Study/src/main/java/com/mvc/spring/sample/config/HibernateConfig.java
@@ -38,6 +38,8 @@ public class HibernateConfig {
         properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+
+        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
         return properties;
     }
 
