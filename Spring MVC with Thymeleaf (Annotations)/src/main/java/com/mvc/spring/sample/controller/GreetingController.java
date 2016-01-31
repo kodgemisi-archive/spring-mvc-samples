00668bfecbd23edfc24f62e814942edd4576d651
Destan Sarpkaya <destan@dorukdestan.com>
2016-01-31 15:16:06 +0200

    hibernate and other configs

--- Spring MVC with Thymeleaf (Annotations)/src/main/java/com/mvc/spring/sample/controller/GreetingController.java
+++ Spring MVC with Thymeleaf (Annotations)/src/main/java/com/mvc/spring/sample/controller/GreetingController.java
@@ -1,8 +1,12 @@
 package com.mvc.spring.sample.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -12,9 +16,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    public String greeting(
+    		@RequestParam(value="name", required=false, defaultValue="World") String name, 
+    		@RequestParam(value="age", required=false, defaultValue="0") int age, 
+    		Model model) {
+    	
+    	name = name + " (comes from url)" + age;
+    	
+        model.addAttribute("person", name);
+        return "basic/greeting";
+    }
+    
+    @RequestMapping(path = "/list", method = RequestMethod.GET)
+    public String list(Model model) {
+  
+    	List<User> users = new ArrayList<>();
+    	users.add(new User("ahmet", 21));
+    	users.add(new User("ayse", 22));
+    	users.add(new User("ebru", 33));
+    	
+    	model.addAttribute("userList", users);
+    	
+        return "list";
     }
 
 }
