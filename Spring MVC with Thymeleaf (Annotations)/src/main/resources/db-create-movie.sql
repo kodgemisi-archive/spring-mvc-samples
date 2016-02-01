95ccced284dfcaa0ebff1c0044d6e4186531ef92
sedooe <sedatgokcen.35@gmail.com>
2016-02-01 11:23:50 +0200

    add db sqls to create default movies

---
+++ Spring MVC with Thymeleaf (Annotations)/src/main/resources/db-create-movie.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Movie (
+  id         INTEGER PRIMARY KEY,
+  title VARCHAR(50),
+  year  INTEGER,
+  director VARCHAR(50)
+);
---
+++ Spring MVC with Thymeleaf (Annotations)/src/main/resources/db-test-movies.sql
@@ -0,0 +1,3 @@
+INSERT INTO Movie(id, title, year, director) VALUES (1, 'Tosun Pasa', 1967, 'Ertem');
+INSERT INTO Movie(id, title, year, director) VALUES (2, 'Hodejegerne', 2013, 'Martin');
+INSERT INTO Movie(id, title, year, director) VALUES (3, 'La Haine', 2012, 'Sayid Reyis');
